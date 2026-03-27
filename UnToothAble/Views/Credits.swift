8e8a2b9d43caf9176985236a261183fdfb9fcea5
Antonio Costa <antonioclaudiocostajr@gmail.com>
2026-03-27 10:28:57 -0300

    Credits created and build updated

--- UnToothAble/Views/Credits.swift
+++ UnToothAble/Views/Credits.swift
@@ -7,39 +7,41 @@
 import SwiftUI
 
 struct CreditsView: View {
-    let onBack: () -> Void
+//    let onBack: () -> Void
 
     var body: some View {
-        VStack(alignment: .leading, spacing: 24) {
-            Button {
-                onBack()
-            } label: {
-                Image(.x)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 35, height: 35)
-                    .offset(x: 610)
+        VStack(alignment: .leading) {
+
+            Spacer()
+            
+
+            Text(" Created with ♥︎ by a team from \n the Apple Developer Academy poa ")
+                .font(.bangers(20))
+                .multilineTextAlignment(.leading)
+                .lineLimit(3)
+                .customStroke(color: .black, width: 1)
+
+            VStack(alignment: .leading){
+                Text(" Antonio costa    -   iOS Developer ")
+                    
+                Text(" Giovana Hossein  -   UX/UI DESIGNER ")
+                    
+                Text(" RAFAEL TONETO    -   iOS Developer ")
+                    
+                Text(" RICHARD FAGUNDES  -   iOS Developer ")
+                    
+                Text(" SOFIA LEITAO    -   iOS Developer ")
+                    
             }
+            .font(.bangers(15))
+            .customStroke(color: .black, width: 0.5)
 
-            Text("Created with 􀊵 by a team from the Apple Developer Academy poa")
-                .font(.bangers(19))
-                .customStroke(color: .black, width: 2)
-
-            Text("""
-            Antonio costa    -   iOS Developer
-            Giovana Hossein  -   UX/UI DESIGNER
-            RAFAEL TONETO    -   iOS Developer
-            RICHARD FAGUNDES  -   iOS Developer
-            SOFIA LEITAO    -   iOS Developer
-            """)
-            .font(.bangers(19))
-            .customStroke(color: .black, width: 1)
             
             Spacer()
         }
-        .padding(24)
+        .frame(width: 400, height: 200)
+        .padding(.top, 24)
         .background(
-            
             Image(.creditsBG)
                 .resizable()
                 .scaledToFill()
@@ -48,12 +50,6 @@ struct CreditsView: View {
 }
 
 #Preview {
-    CreditsView() {
-        print("")
-    }
-    .background(
-        Color.black
-            .ignoresSafeArea()
-        
-    )
+    CreditsView()
+    
 }
